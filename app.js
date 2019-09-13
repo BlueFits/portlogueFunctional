a2d427998612f6dbde03397809bd4f36ecb566fd
Christian Rojas <christianportlogue@gmail.com>
2019-09-13 13:28:34 -0400

    Production ready

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ const flash = require(`connect-flash`);
 const session = require(`express-session`);
 const passport = require(`passport`);
 
+//For productions modules
+const compression = require(`compression`);
+const helmet = require(`helmet`);
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -26,6 +30,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(compression());
+app.use(helmet());
 
 //Express Session Middleware
 app.use(session({
